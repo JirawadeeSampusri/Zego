e418a52ceedc1566bce606d8f1de0e973dbc0682
Jirawadee Sampusri <jirawadeelovely@hotmail.com>
2019-04-08 00:19:17 +0700

    fix bullet

--- oofake.py
+++ oofake.py
@@ -7,7 +7,7 @@ from Zego_models import World
 SPRITE_SCALING_PLAYER = 0.8
 SPRITE_SCALING_COIN = 0.5
 SPRITE_SCALING_LASER = 0.8
-COIN_COUNT = 50
+COIN_COUNT = 30
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -89,7 +89,7 @@ class ZegoDotWindow(arcade.Window):
         self.score = 0
         self.score_text = None
 
-        # self.set_mouse_visible(False)
+        self.set_mouse_visible(False)
 
         arcade.set_background_color(arcade.color.BABY_BLUE_EYES)
 
@@ -176,6 +176,11 @@ class ZegoDotWindow(arcade.Window):
             if bullet.bottom > self.width or bullet.top < 0 or bullet.right < 0 or bullet.left > self.width:
                 bullet.kill()
 
+    def on_mouse_motion(self, x, y, dx, dy):
+        """
+        Called whenever the mouse moves.
+        """
+        self.player_sprite.center_x = x
 
     def on_mouse_press(self, x, y, button, modifiers):
         """
@@ -184,54 +189,28 @@ class ZegoDotWindow(arcade.Window):
         # Create a bullet
         bullet = arcade.Sprite("images/laser3.png", SPRITE_SCALING_LASER)
 
-        # Position the bullet at the player's current location
         start_x = self.player_sprite.center_x
         start_y = self.player_sprite.center_y
         bullet.center_x = start_x
         bullet.center_y = start_y
 
-        # Get from the mouse the destination location for the bullet
-        # IMPORTANT! If you have a scrolling screen, you will also need
-        # to add in self.view_bottom and self.view_left.
         dest_x = x
         dest_y = y
 
-        # Do math to calculate how to get the bullet to the destination.
-        # Calculation the angle in radians between the start points
-        # and end points. This is the angle the bullet will travel.
         x_diff = dest_x - start_x
         y_diff = dest_y - start_y
         angle = math.atan2(y_diff, x_diff)
 
-        # Angle the bullet sprite so it doesn't look like it is flying
-        # sideways.
+       
         bullet.angle = math.degrees(angle)
         print(f"Bullet angle: {bullet.angle:.2f}")
 
-        # Taking into account the angle, calculate our change_x
-        # and change_y. Velocity is how fast the bullet travels.
+       
         bullet.change_x = math.cos(angle) * BULLET_SPEED
         bullet.change_y = math.sin(angle) * BULLET_SPEED
 
         # Add the bullet to the appropriate lists
         self.bullet_list.append(bullet)
-
-    # def on_key_press(self, key, modifiers):
-    #     """Called whenever a key is pressed. """
-
-    #     if key == arcade.key.LEFT:
-    #         self.player_sprite.change_x = -MOVEMENT_SPEED
-    #     elif key == arcade.key.RIGHT:
-    #         self.player_sprite.change_x = MOVEMENT_SPEED
-
-    # def on_key_release(self, key, modifiers):
-    #     """Called when the user releases a key. """
-
-    #     if key == arcade.key.UP or key == arcade.key.DOWN:
-    #         self.player_sprite.change_y = 0
-    #     elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
-    #         self.player_sprite.change_x = 0
-
 
 def main():
     """ Main method """
